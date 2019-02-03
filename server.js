1560d64d95c54e9d0a13710477be2d3953db87ff
reonardoleis <leonardosilvabf@gmail.com>
2019-02-02 22:45:38 -0200

    First commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.port || 3000;
+const app = express();
+const server = require('http').createServer(app); 
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+	res.render('index.html');
+});
+
+var users = [];
+var messages = [];
+
+io.on('connection', socket => {
+	
+	socket.emit('previousMessages', messages);
+	socket.on('newUser', data => {
+		users.push(data);
+		socket.username = data;
+		socket.broadcast.emit('addUser', users);
+		socket.emit('connectedUsers', users);
+	});
+	socket.on('sendMessage', data => {
+		socket.broadcast.emit('receivedMessage', data);
+	});
+	socket.on('disconnect', function(){
+		users.splice(users.indexOf(socket.username), 1);
+		socket.broadcast.emit('connectedUsers', users);
+		if(socket.username!=undefined){
+			messages.push({author: '', message: '<strong>'+socket.username+ ' has left the chat.</strong>'});
+			socket.broadcast.emit('previousMessages', messages);
+			socket.username = undefined;
+		}
+	})
+
+});
+
+server.listen(PORT);
